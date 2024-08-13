981b14d762976a5fa8a32538a6ae50636a6ea9a3
말랑곰 <leehoje000@gmail.com>
2024-08-13 14:48:09 +0900

    refactor: 지역변수 total_price 사용안하도록 수정
    ItemList클래스에있는 total_price변수를 사용하도록 수정

--- check_menu.py
+++ check_menu.py
@@ -2,11 +2,7 @@ from itemlist import ItemList
 
 def check_menu(item_list: ItemList):
     
-    total_price = 0
-    for menu in item_list.add_menu:
-        total_price += item_list.menu[menu]
-
-    print(f'주문 품목 총액 {total_price}입니다.')
+    print(f'주문 품목 총액 {item_list.total_price}입니다.')
     print('=⇒', end='')
     # for menu in item_list.add_menu:
     print(*item_list.add_menu, sep=', ', end='')
